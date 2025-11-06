58c431b50197f77a713680ab6306a9fb3b4af663
alexdrvg <you@example.com>
2025-11-06 19:22:24 +0100

    prueba 2

---
+++ src/AEA3/test.java
@@ -0,0 +1,11 @@
+package AEA3;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Hola, ¿Cómo te llamas?");
+        System.out.println("Encantada de conocerte "+s.nextLine());
+    }
+}
