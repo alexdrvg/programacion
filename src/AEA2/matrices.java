61897c910a73ac9db829db13871aeca25e3c53d3
alexdrvg <you@example.com>
2025-11-10 20:39:40 +0100

    10/11/2025

---
+++ src/AEA2/matrices.java
@@ -0,0 +1,21 @@
+package AEA2;
+
+public class matrices {
+    public static void main(String[] args) {
+        int [][] matriz = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        
+        System.out.println("Elementos de la matriz: ");
+
+        for (int i=0; i<matriz.length; i++) {
+            for (int j=0; j<matriz[i].length;j++) {
+                System.out.println(matriz[i][j]+"");
+            }
+            System.out.println();
+        }
+
+    }
+}
