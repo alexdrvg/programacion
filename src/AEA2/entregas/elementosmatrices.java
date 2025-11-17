0b7e3e0d2d5d0308666fd0d8184647c196dead18
alexdrvg <you@example.com>
2025-11-17 20:51:14 +0100

    ahora si que si

--- src/AEA2/entregas/elementosmatrices.java
+++ src/AEA2/entregas/elementosmatrices.java
@@ -1,6 +1,5 @@
 package AEA2.entregas;
 import java.util.Scanner; 
-// import java.util.Arrays; 
 
 public class elementosmatrices {
     public static void main(String[] args) {
@@ -13,12 +12,12 @@ public class elementosmatrices {
         int columnas = s.nextInt(); 
 
         int[][] matrices = new int [filas][columnas]; 
-
+        
         for (int i=0; i<matrices.length; i++) {
 
             for (int j=0; j<matrices[i].length; j++) {
 
-                System.out.println("Introduce un numero para la fila "+i+" y la columna "+j);
+                System.out.println("Introduce un numero para la fila "+i+" y la columna "+j+": ");
                 matrices[i][j] = s.nextInt(); 
 
             }
@@ -34,6 +33,23 @@ public class elementosmatrices {
             System.out.println();
         }
 
+        int maximo = matrices[0][0];
+        int minimo = matrices[0][0]; 
+
+        for (int i=0; i<matrices.length; i++) {
+            for (int j=0; j<matrices[i].length; j++) {
+                if (maximo < matrices[i][j]) {
+                    maximo = matrices [i][j];
+                }
+                
+                if (minimo > matrices[i][j]){
+                    minimo = matrices[i][j]; 
+                }
+            }
+        }
+
+        System.out.println("El número más grande de la matriz es el: "+maximo);
+        System.out.println("El número más pequeño de la matriz es el: "+minimo);
 
     }
 }
